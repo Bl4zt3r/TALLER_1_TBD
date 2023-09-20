45705c07aaa1c7dc292501b7c1c6e19f2f97e0f0
Bl4zt3r <89836916+Bl4zt3r@users.noreply.github.com>
2023-09-20 18:50:00 -0300

    actualizando query 2

--- runStatements.sql
+++ runStatements.sql
@@ -16,13 +16,13 @@ ORDER BY cantidad_viajes DESC;
 -- pregunta 2
 
 
-SELECT s.nombre, COUNT(DISTINCT p.id_pasaje) AS cantidad_compras
-FROM cliente c
-JOIN cliente_vuelo cv ON c.rut = cv.rut
-JOIN vuelo v ON cv.id_vuelo = v.id_vuelo
-JOIN pasaje p ON v.id_vuelo = p.id_vuelo
-JOIN seccion s ON p.id_seccion = s.id_seccion
+SELECT s.nombre, COUNT(*) as total_comprado
+FROM pasaje AS p
+JOIN cliente AS c ON p.rut = c.rut
+JOIN vuelo AS v ON p.id_vuelo = v.id_vuelo
+JOIN seccion AS s ON p.id_seccion = s.id_seccion
 WHERE c.nacionalidad = 'Argentino'
 GROUP BY s.nombre
-ORDER BY cantidad_compras DESC
+ORDER BY total_comprado DESC;
+
 
