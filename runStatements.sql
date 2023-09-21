968af7c328ff1a59408caf241874bc2a9f028a13
Bl4zt3r <89836916+Bl4zt3r@users.noreply.github.com>
2023-09-20 22:38:44 -0300

    consultas actualizadas

--- runStatements.sql
+++ runStatements.sql
@@ -16,13 +16,13 @@ ORDER BY cantidad_viajes DESC;
 -- pregunta 2
 
 
-SELECT s.nombre, COUNT(*) as total_comprado
+SELECT s.nombre_seccion, COUNT(*) as total_comprado
 FROM pasaje AS p
 JOIN cliente AS c ON p.rut = c.rut
 JOIN vuelo AS v ON p.id_vuelo = v.id_vuelo
 JOIN seccion AS s ON p.id_seccion = s.id_seccion
 WHERE c.nacionalidad = 'Argentino'
-GROUP BY s.nombre
+GROUP BY s.nombre_seccion
 ORDER BY total_comprado DESC;
 
 
@@ -46,8 +46,8 @@ ORDER BY
 
 ----------/Pregunta 4/----------
 SELECT DISTINCT
-    Cliente.nombre AS nombre,
-	COUNT(*) AS total
+  Cliente.nombre_cliente AS nombre,
+  COUNT(*) AS total
 FROM
     Pasaje
 JOIN
@@ -56,9 +56,9 @@ JOIN
     Seccion ON Pasaje.id_seccion = Seccion.id_seccion
 WHERE
     Seccion.nombre = 'Primera Clase' 
-	AND DATE_PART('month', Pasaje.fechaInicio) = DATE_PART('month', Pasaje.fechaLlegada)
+  AND DATE_PART('month', Pasaje.fechaInicio) = DATE_PART('month', Pasaje.fechaLlegada)
 GROUP BY
-    Cliente.nombre
+    Cliente.nombre_cliente
 HAVING
     COUNT(Pasaje.fechaInicio) > 4;
 
@@ -90,30 +90,86 @@ WHERE e.cargo = 'Piloto'
 ORDER BY s.monto DESC, s.ano DESC, s.mes DESC;
 
 
+-- pregunta 7
+
+
+CREATE OR REPLACE view v_pasaje1  as
+select compania_vuelo.id_compania, monto
+from pasaje
+join compania_vuelo on pasaje.id_vuelo=compania_vuelo.id_vuelo
+join costo on pasaje.id_costo=costo.id_costo
+where EXTRACT(YEAR FROM pasaje.fechaPasaje) >= 2019 and EXTRACT(YEAR FROM pasaje.fechaPasaje)<2024;
+
+
+CREATE OR REPLACE view  v_avion  as
+select nombre_modelo, nombre_compania, avion.id_compania
+from avion
+join modelo on modelo.id_modelo= avion.id_modelo
+join compania on compania.id_compania=avion.id_compania;
+
+
+select v_avion.nombre_compania, v_avion.nombre_modelo , sum (monto) 
+from V_avion
+join v_pasaje1 on v_pasaje1.id_compania= v_avion.id_compania
+group by v_avion.nombre_modelo , v_avion.nombre_compania, v_avion.id_compania
+order by (v_avion.id_compania) asc; 
+
+
+
 -- pregunta 8
 
 
-SELECT co.nombre, EXTRACT (YEAR FROM fechafabricacion) as ano, COUNT(*) AS cantidad_aviones
+SELECT co.nombre_compania, EXTRACT (YEAR FROM fechafabricacion) as ano, COUNT(*) AS cantidad_aviones
 FROM avion
 join avion_vuelo as av on av.patente = avion.patente
 join compania_vuelo as cv on cv.id_vuelo = av.id_vuelo
 join compania co on co.id_compania = cv.id_compania
 WHERE EXTRACT (YEAR FROM fechafabricacion) > EXTRACT (YEAR FROM CURRENT_DATE) - 10 
-group by co.nombre, fechafabricacion
+group by co.nombre_compania, fechafabricacion
+
 
 
 -- pregunta 9
 
 
 SELECT nombre, ano, SUM(monto) FROM
-(SELECT co.nombre,s.monto, s.ano, COUNT(*) as cantidad_empleados
+(SELECT co.nombre_compania,s.monto, s.ano, COUNT(*) as cantidad_empleados
 from empleado as e
 join sueldo as s on e.id_sueldo = s.id_sueldo
 join empleado_vuelo as es on e.id_empleado = es.id_empleado
 join compania_vuelo as cv on es.id_vuelo = cv.id_vuelo
 join compania as co on cv.id_compania = co.id_compania
 WHERE s.ano > EXTRACT (YEAR FROM CURRENT_DATE) - 10
-GROUP BY co.nombre, s.monto, s.ano
-ORDER BY co.nombre DESC) AS r1
+GROUP BY co.nombre_compania, s.monto, s.ano
+ORDER BY co.nombre_compania DESC) AS r1
 GROUP BY nombre, ano
 ORDER BY ano DESC;
+
+-- Pregunta 10
+SELECT
+    modelo.id_modelo,
+    modelo.nombre_modelo AS nombre_modelo,
+    COUNT(*) AS cantidad_utilizado
+FROM
+    avion_vuelo 
+JOIN
+    (SELECT DISTINCT compania_vuelo.id_vuelo
+     FROM compania_vuelo 
+     WHERE compania_vuelo.id_compania = 2) cv2
+ON
+    avion_vuelo.id_vuelo = cv2.id_vuelo
+JOIN
+    avion  ON avion_vuelo.patente = avion.patente
+JOIN
+    modelo  ON avion.id_modelo = modelo.id_modelo
+JOIN
+    vuelo  ON avion_vuelo.id_vuelo = vuelo.id_vuelo
+JOIN
+    pasaje  ON vuelo.id_vuelo = pasaje.id_vuelo
+WHERE
+    EXTRACT(YEAR FROM pasaje.fechaPasaje) = 2023
+GROUP BY
+    modelo.id_modelo, modelo.nombre_modelo
+ORDER BY
+    cantidad_utilizado DESC
+LIMIT 1;
