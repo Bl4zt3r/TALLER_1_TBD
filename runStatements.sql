05ed19bc9d4c6543a03e0bf212b412213e51f299
Yuyunisse <92490217+Yuyunisse@users.noreply.github.com>
2023-09-20 21:25:34 -0300

    Update runStatements.sql

--- runStatements.sql
+++ runStatements.sql
@@ -26,3 +26,30 @@ GROUP BY s.nombre
 ORDER BY total_comprado DESC;
 
 
+-- pregunta 8
+
+
+SELECT co.nombre, EXTRACT (YEAR FROM fechafabricacion) as ano, COUNT(*) AS cantidad_aviones
+FROM avion
+join avion_vuelo as av on av.patente = avion.patente
+join compania_vuelo as cv on cv.id_vuelo = av.id_vuelo
+join compania co on co.id_compania = cv.id_compania
+WHERE EXTRACT (YEAR FROM fechafabricacion) > EXTRACT (YEAR FROM CURRENT_DATE) - 10 
+group by co.nombre, fechafabricacion
+
+
+-- pregunta 9
+
+
+SELECT nombre, SUM(monto) FROM
+(SELECT co.nombre,s.monto, COUNT(*) as cantidad_empleados
+from empleado as e
+join sueldo as s on e.id_sueldo = s.id_sueldo
+join empleado_vuelo as es on e.id_empleado = es.id_empleado
+join compania_vuelo as cv on es.id_vuelo = cv.id_vuelo
+join compania as co on cv.id_compania = co.id_compania
+WHERE s.ano > EXTRACT (YEAR FROM CURRENT_DATE) - 10
+GROUP BY co.nombre, s.monto
+ORDER BY co.nombre DESC) AS r1
+GROUP BY nombre
+ORDER BY sum DESC;
