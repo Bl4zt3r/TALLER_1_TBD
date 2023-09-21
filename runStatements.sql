8b2797f6b174f9abe43ba03c133b4b68412d16b5
zth-Felipe <92702735+zth-Felipe@users.noreply.github.com>
2023-09-20 22:33:26 -0300

    Corrección pregunta 9

--- runStatements.sql
+++ runStatements.sql
@@ -105,15 +105,15 @@ group by co.nombre, fechafabricacion
 -- pregunta 9
 
 
-SELECT nombre, SUM(monto) FROM
-(SELECT co.nombre,s.monto, COUNT(*) as cantidad_empleados
+SELECT nombre, ano, SUM(monto) FROM
+(SELECT co.nombre,s.monto, s.ano, COUNT(*) as cantidad_empleados
 from empleado as e
 join sueldo as s on e.id_sueldo = s.id_sueldo
 join empleado_vuelo as es on e.id_empleado = es.id_empleado
 join compania_vuelo as cv on es.id_vuelo = cv.id_vuelo
 join compania as co on cv.id_compania = co.id_compania
 WHERE s.ano > EXTRACT (YEAR FROM CURRENT_DATE) - 10
-GROUP BY co.nombre, s.monto
+GROUP BY co.nombre, s.monto, s.ano
 ORDER BY co.nombre DESC) AS r1
-GROUP BY nombre
+GROUP BY nombre, ano
 ORDER BY sum DESC;
